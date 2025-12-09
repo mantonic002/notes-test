208309a1b61a629d5b9a2fcca2b982fc23daa89e
mantonic002 <mantonic002@gmail.com>
2025-12-09 14:10:17 +0100

    feat: add notes module and implement create and list notes with in-memory storage

---
+++ notes-backend/src/notes/entities/note.entity.ts
@@ -0,0 +1,5 @@
+export class Note {
+  note: string;
+  user_id?: number;
+  group?: string;
+}
---
+++ notes-backend/src/notes/notes.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { CreateNoteDto } from './dto/create-note.dto';
+import { UpdateNoteDto } from './dto/update-note.dto';
+import { Note } from './entities/note.entity';
+
+@Injectable()
+export class NotesService {
+  private readonly notes: Note[] = [];
+
+  create(createNoteDto: CreateNoteDto) {
+    const note: Note = { note: createNoteDto.note };
+    this.notes.push(note);
+    return note;
+  }
+
+  findAll() {
+    return this.notes;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} note`;
+  }
+
+  update(id: number, updateNoteDto: UpdateNoteDto) {
+    return `This action updates a #${id} note`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} note`;
+  }
+}
