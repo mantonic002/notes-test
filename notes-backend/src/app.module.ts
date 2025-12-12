8c9e4faad7c83114a90403925b24574a4b5e4b0b
mantonic002 <mantonic002@gmail.com>
2025-12-12 20:20:53 +0100

    chore: delete premade controller and service from app module

--- notes-backend/src/app.module.ts
+++ notes-backend/src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { NotesModule } from './notes/notes.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [NotesModule, MongooseModule.forRoot('mongodb://localhost:27017'), AuthModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
